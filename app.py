c43670ed068b7a5147c05bf274a20b0771090304
Bushra Hanif <160732251+BushraHani@users.noreply.github.com>
2025-04-30 12:38:54 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+
+# Mad Libs Story ===== Python Project ======
+name = input('Enter Name:  ')
+age = input('how old: ')
+profession = input('tell us a job: ')
+topics = input('which are topics: ')
+feeling = input('how do you feel? ')
+features = input('what you want show off features? ')
+media = input('what is feild? ')
+fact = input('what you want show off? ')
+feeling2 =  input('what you want share? ')
+
+
+# Mad lib story
+print(f"Hey, I am {name},")
+print(f"i am {age} years old and a {profession}")
+print(f"who {feeling} diving deep into stories that shape our world.")
+print(f"Whether it is investigative pieces, breaking news, or human-interest features,")
+print(f"I am all about bringing {fact} to light and giving a voice to important issues.")
+print(f"I have written for {media} and covered everything from {topics}.")
+print(f"{feeling2} to share stories that matter!")
+
+      
